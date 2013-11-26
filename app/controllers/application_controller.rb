3f319fbc8eb82493f542e07e83da33f354d2da17
Alan Carl Brown <syracal@gmail.com>
2013-11-25 16:38:09 -0800

    Added the before filter required to make cancan work with Rails 4.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # So that CanCan works with a hash already cleaned up by strong parameters
+  before_filter do
+    # Get a symbol for the name of the resource from the controller name
+    resource = controller_path.singularize.gsub('/', '_').to_sym
+
+    # Determine the name of the strong parameters method
+    method = "#{resource}_params"
+
+    # Use the hash from strong parameters if its set up
+    params[resource] &&= send(method) if respond_to?(method, true)
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
